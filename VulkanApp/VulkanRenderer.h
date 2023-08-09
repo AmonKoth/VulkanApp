f2701e84bfabea8a3d5a5efa5807bf04e05c40a1
Can A <26111053+AmonKoth@users.noreply.github.com>
2023-08-09 16:00:03 +0200

    Validation Layer

--- VulkanApp/VulkanRenderer.h
+++ VulkanApp/VulkanRenderer.h
@@ -34,18 +34,44 @@ private:
 
 	VkQueue graphicsQueue;
 
+	const std::vector<const char*> validationLayers = {
+		"VK_LAYER_KHRONOS_validation"
+	};
+
+	#ifdef NDEBUG
+		const bool enableValidationLayers = false;
+	#else
+		const bool enableValidationLayers = true;
+	#endif // NDEBUG
+	
+	VkDebugUtilsMessengerEXT debugMessenger;
+
+
 	//Vulkan Functions:
 	void createInstance();
+	void populateDebugMessengerCreateInfo(VkDebugUtilsMessengerCreateInfoEXT& createInfo);
 	void createLogicalDevice();
+	void setupDebugMessenger();
 
+	VkResult CreateDebugUtilsMessengerEXT(VkInstance instance, const VkDebugUtilsMessengerCreateInfoEXT* pCreateInfo,
+		const VkAllocationCallbacks* pAllocator, VkDebugUtilsMessengerEXT* pDebugMessenger);
 
-
+	void DestroyDebugUtilsMessengerEXT(VkInstance instance, VkDebugUtilsMessengerEXT debugMessenger, const VkAllocationCallbacks* pAllocator);
 	//Get Funcitons
 	void getPysicalDevice();
+	std::vector<const char*> getRequriredExtensions();
 
 	//Support Functions
 	bool checkInstanceExtensionSupport(std::vector<const char*>* checkExtensions);
 	bool checkDeviceSuitable(VkPhysicalDevice device);
+	bool checkValidationLayerSupport();
+	static VKAPI_ATTR VkBool32 VKAPI_CALL debugCallback(VkDebugUtilsMessageSeverityFlagBitsEXT messageSverity,
+		VkDebugUtilsMessageTypeFlagsEXT messageType,
+		const VkDebugUtilsMessengerCallbackDataEXT* pCallbackData,
+		void* pUserData) {
+		printf("Validation layer: %s \n", pCallbackData->pMessage);
+		return VK_FALSE;
+	}
 
 	QueueFamilyIndices getQueueFamiles(VkPhysicalDevice device);
 
